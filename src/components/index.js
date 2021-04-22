2a6e3cf8395bfc77fc93d27a27be0359f8b3773a
Jonathan Soukaseume <sksmejn@gmail.com>
2021-04-21 18:04:26 -0700

    adding boilerplate for specific site

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import { default as Navbar } from "./Navbar";
+import { default as Home } from "./Home";
+import { default as About } from "./About";
+import { default as Footer } from "./Footer";
+
+export { Navbar, Home, About, Footer };
