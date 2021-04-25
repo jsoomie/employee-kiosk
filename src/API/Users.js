d4883d6f663e3e2d3e63992809fd483b2d146514
Jonathan Soukaseume <sksmejn@gmail.com>
2021-04-25 13:23:26 -0700

    Updates

--- src/API/Users.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const userList = () => {
-  return axios.get("https://randomuser.me/api/?results=30&nat=US");
-};
--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import { default as Navbar } from "./Navbar";
-import { default as Home } from "./Home";
-import { default as Footer } from "./Footer";
-import { default as Search } from "./Search";
-import { default as Card } from "./Card";
-import { default as SortButton } from "./SortButton";
-
-export { Navbar, Home, Footer, Search, Card, SortButton };
